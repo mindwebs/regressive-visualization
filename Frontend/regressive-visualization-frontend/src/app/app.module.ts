e44a75a0d66530a59ce4391ae8b6f779e4bda075
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-05-09 23:42:22 +0530

    added backend and ml code

--- Frontend/regressive-visualization-frontend/src/app/app.module.ts
+++ Frontend/regressive-visualization-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ParticlesModule } from 'angular-particle';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ParticlesModule
+    ParticlesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/regressive-visualization-frontend/src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  myParams: object = {};
+  myStyle: object = {};
+  closeResult = '';
+  
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {
+    this.myParams = {
+      particles: {
+        number: {
+          value: 300,
+        }
+      }
+    };
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+}
